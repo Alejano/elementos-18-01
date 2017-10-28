c7d8430788b43ee823dfa9858c39afd4720cf78c
T-107 <T-107@PC186>
2017-10-27 21:42:31 -0500

    creamos otras tablas usuario y direccion con relacion uno a uno

--- src/main/java/org/unitec/elementos1801/tablitas.sql
+++ src/main/java/org/unitec/elementos1801/tablitas.sql
@@ -1,5 +1,14 @@
 create table mensajito(id integer primary key auto_increment,titulo varchar(50),cuerpo varchar(250));
 
+create table usuario(cuenta integer primary key ,nombre varchar(40),email varchar(40));
+
+create table direccion(id_dir integer primary key auto_increment,cuenta integer,
+calle varchar(100), cp integer, municipio varchar(40), foreign key (cuenta) references usuario(cuenta));
+
+
 describe mensajito;
+describe usuario;
+describe direccion;
 
 select *from mensajito;
+
